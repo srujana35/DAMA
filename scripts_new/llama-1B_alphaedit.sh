55abf58bf37b523b06428f15690fe47ded4eee0d
srujana35 <srujanapillarichety@gmail.com>
2025-05-12 01:02:10 +0000

    scripts

---
+++ scripts_new/llama-1B_alphaedit.sh
@@ -0,0 +1,6 @@
+python adapt_model.py --model_name "meta-llama/Llama-3.2-1B" --method "DAMA" --request_file "train_dama_tiny_processed.json" --num_layers 2 --post_linear True --iterative_update True --random_seed 45
+
+# python evaluate_model.py --model_name "meta-llama/Llama-3.2-1B" --num_layers 2 --method "MEMIT" --test_file test_dama.json --test_task gen --post_linear True --iterative_update True --random_seed 45
+# python evaluate_model.py --model_name "meta-llama/Llama-3.2-1B" --num_layers 0 --method "MEMIT" --test_file "anti_type1_test.txt" --test_task coref --post_linear True --iterative_update True --random_seed 45
+# python evaluate_model.py --model_name "meta-llama/Llama-3.2-1B" --num_layers 2 --method "MEMIT" --test_file stereoset_dev.json --test_task stereoset --post_linear True --iterative_update True --random_seed 45
+# python evaluate_model.py --model_name "meta-llama/Llama-3.2-1B" --num_layers 2 --method "MEMIT" --test_file wikitext_wikitext-2-raw-v1 --test_task "causal_lm" --post_linear True --iterative_update True --random_seed 45
--- scripts_new/llama-tiny_alphaedit.sh
+++ scripts_new/llama-tiny_alphaedit.sh
@@ -1,4 +1,4 @@
-python adapt_model.py --model_name "HuggingFaceM4/tiny-random-LlamaForCausalLM" --method "DAMA" --request_file "train_dama_tiny.json" --num_layers 2 --post_linear True --iterative_update True --random_seed 45
+python adapt_model.py --model_name "HuggingFaceM4/tiny-random-LlamaForCausalLM" --method "DAMA" --request_file "train_dama_tiny_processed.json" --num_layers 2 --post_linear True --iterative_update True --random_seed 45
 
 # python evaluate_model.py --model_name "HuggingFaceM4/tiny-random-LlamaForCausalLM" --num_layers 2 --method "DAMA" --test_file test_dama.json --test_task gen
 # python evaluate_model.py --model_name "HuggingFaceM4/tiny-random-LlamaForCausalLM" --num_layers 0 --method "MEMIT" --test_file "anti_type1_test.txt" --test_task coref
