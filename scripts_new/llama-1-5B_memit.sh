236d7cc260a9b5f55c54236fdfbabf4ba2233496
srujana35 <srujanapillarichety@gmail.com>
2025-05-12 00:47:07 +0000

    filename

--- scripts_new/llama-1-5B_memit.sh
+++ scripts_new/llama-1-5B_memit.sh
@@ -1,4 +1,4 @@
-python adapt_model.py --model_name "meta-llama/Llama-3.2-1B" --method "MEMIT" --request_file "train_dama_processed.json" --num_layers 2 --post_linear True --iterative_update True --random_seed 45
+python adapt_model.py --model_name "meta-llama/Llama-3.2-1B" --method "MEMIT" --request_file "train_dama.json" --num_layers 2 --post_linear True --iterative_update True --random_seed 45
 
 # python evaluate_model.py --model_name "meta-llama/Llama-3.2-1B" --num_layers 2 --method "MEMIT" --test_file test_dama.json --test_task gen --post_linear True --iterative_update True --random_seed 45
 # python evaluate_model.py --model_name "meta-llama/Llama-3.2-1B" --num_layers 2 --method "MEMIT" --test_file "anti_type1_test.txt" --test_task coref --post_linear True --iterative_update True --random_seed 45
